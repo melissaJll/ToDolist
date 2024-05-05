3ff8a2e039d3722a7226531579a6cc24e2c978b6
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 16:19:51 -0300

    porta como variavel

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`servidor rodadando em http://localhost:${port}`)
+})
