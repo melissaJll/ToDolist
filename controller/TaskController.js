01e3d8400392a31bd576b26cb48b1b5eb66932bb
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 16:41:47 -0300

    local das rotas e crud

---
+++ controller/TaskController.js
@@ -0,0 +1,7 @@
+//Funções CRUD
+
+const getAll = (req,res) => {
+    return res.render("index");
+}
+
+export {getAll}
--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const port = 3000;
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "publica")))
 
+
+//Rotas (Principal)
 app.get('/', function (req, res) {
     //nome da pasta ejs
   res.render("index")
 })
+
 
 app.listen(port, () => {
     console.log(`servidor rodadando em http://localhost:${port}`)
