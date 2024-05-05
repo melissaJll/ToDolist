765645fb904ccf561cc8baa55922bc1c83d8dfc8
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 17:15:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const path = require("path")
+const express = require("express");
+const path = require("path");
+//import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, "publica")))
-
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "publica")));
 
 //Rotas (Principal)
-app.get('/', function (req, res) {
-    //nome da pasta ejs
-  res.render("index")
-})
-
+app.get("/", function (req, res) {
+  //nome da pasta ejs
+  res.render("index");
+});
 
 app.listen(port, () => {
-    console.log(`servidor rodadando em http://localhost:${port}`)
-})
+  console.log(`servidor rodadando em http://localhost:${port}`);
+});
