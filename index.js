0cab0fffb9015f752ba5e17c4c0b18c4dd98eaa8
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 20:25:19 -0300

    estudar está linha

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const conexaoDB = require('./banco/db')
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "publica")));
 
