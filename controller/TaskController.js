77a6a52853149f98fa5fbcedc4e50ca86ccf6cce
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 20:25:57 -0300

    formulario funcionando com banco de dados

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,9 +1,38 @@
+const conexao = require('../banco/db')
+
 //Funções CRUD
 
-const getAll = (req,res) => {
-    return res.render("index");
-}
+const getAll = (req, res) => {
+  return res.render("index");
+};
+
+
+const inserirTarefa = (req, res) => {
+    const nomeTarefa = req.body.nomeTarefa;
+    const dataCriacao = req.body.dataCriacao;
+    // Convertendo o valor do checkbox para booleano
+    const statusTarefa = req.body.statusTarefa ? true : false;
+    const prioridade = req.body.prioridade;
+    
+    const sql = `INSERT INTO Tarefas (nomeTarefa, dataCriacao, statusTarefa, prioridade) VALUES (?, ?, ?, ?)`;
+
+    conexao.query(sql, [nomeTarefa, dataCriacao, statusTarefa, prioridade], (error, results) => {
+        if (error) {
+            return res.status(500).send({
+                success: false,
+                message: "Erro ao inserir tarefa: " + error
+            });
+        }
+        return res.status(201).send({
+            success: true,
+            message: "Tarefa inserida com sucesso!",
+            data: results
+        });
+    });
+};
 
 module.exports = {
-    getAll
-}
+  getAll,
+  inserirTarefa,
+};
+
--- rotas/routes.js
+++ rotas/routes.js
@@ -1,7 +1,13 @@
 const routes = require("express").Router();
+const express = require("express");
+const app = express();
+
 //import { getAll } from "../controller/TaskController";
 const TaskController = require("../controller/TaskController")
 
 routes.get("/", TaskController.getAll);
+//routes.post("/criar", TaskController.createTask)
+routes.post("/inserir", TaskController.inserirTarefa);
 
+ 
 module.exports = routes;
