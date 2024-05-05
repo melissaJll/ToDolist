f76347bf81eff8691f375ecdd9d2eb31c3550171
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-05 17:28:32 -0300

    rotas com routes

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+//import { routes } from "./rotas/routes";
 const express = require("express");
 const path = require("path");
-//import path from 'path';
+const routes = require("./rotas/routes")
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "publica")));
-
-//Rotas (Principal)
-app.get("/", function (req, res) {
-  //nome da pasta ejs
-  res.render("index");
-});
+//Substituindo rota
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`servidor rodadando em http://localhost:${port}`);
