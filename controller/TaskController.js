7938b81cbc34ae19f27dbdbc775b6a38563145a5
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-09 15:50:59 -0300

    delete

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -11,7 +11,6 @@ const getAll = async (req, res) => {
   }
 };
 
-
 const createTask = async (req, res) => {
   const task = req.body;
 
@@ -26,29 +25,40 @@ const createTask = async (req, res) => {
   }
 };
 
-
 const getById = async (req, res) => {
-    try {
-      const task = await Task.findOne({ _id: req.params.id });
-      const tasksList = await Task.find();
-      res.render("index", { task, tasksList });
-    } catch (err) {
-      res.status(500).send({ error: err.message });
-    }
-  };
-  
-  const updateTask = async (req, res) =>{
-      try {
-      const task = req.body;
-      await Task.updateOne({_id: req.params.id}, task)
-      res.redirect("/")
-      } catch (err) {
-          res.status(500).send({ error: err.message });
-        }
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    res.render("index", { task, tasksList });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
   }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const taskDelete = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    res.render("index", { task: null, tasksList });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
 
 module.exports = {
   getAll,
   createTask,
-  getById, updateTask
+  getById,
+  updateTask,
+  deleteTask,
 };
