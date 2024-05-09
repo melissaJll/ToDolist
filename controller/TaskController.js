af327f4d56e572e1725229fc621741c3773be0ea
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-09 11:48:03 -0300

    retirando checkbox

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,50 +1,48 @@
-const conexao = require('../banco/db');
-
+const conexao = require("../banco/db");
 
 //Funções CRUD
 // const getAll = (req, res) => {
 //   return res.render("index");
 // };
-
-
 const inserirTarefa = (req, res) => {
-    const nomeTarefa = req.body.nomeTarefa;
-    const dataCriacao = req.body.dataCriacao;
-    // Convertendo o valor do checkbox para booleano
-    const statusTarefa = req.body.statusTarefa ? true : false;
-    const prioridade = req.body.prioridade;
+  const nomeTarefa = req.body.nomeTarefa;
+  const dataCriacao = req.body.dataCriacao;
+  const prioridade = req.body.prioridade;
 
-    const sql = `INSERT INTO Tarefas (statusTarefa, nomeTarefa, dataCriacao,  prioridade) VALUES (?, ?, NOW(),  ?)`;
-
-    conexao.query(sql, [ statusTarefa, nomeTarefa, dataCriacao, prioridade], (error, results) => {
-        if (error) {
-            return res.status(500).send({
-                success: false,
-                message: "Erro ao inserir tarefa: " + error
-            });
-        }
-        return res.status(201).send({
-            success: true,
-            message: "Tarefa inserida com sucesso!",
-            data: results
-        });
+  // Certifique-se de que o valor de 'prioridade' corresponda a 'Alta', 'Média' ou 'Baixa'
+  if (!["Alta", "Média", "Baixa"].includes(prioridade)) {
+    return res.status(400).send({
+      message: "Erro: Valor de 'prioridade' inválido.",
     });
+  }
+
+  const sql = `INSERT INTO Tarefas (nomeTarefa, dataCriacao, prioridade) VALUES (?, NOW(), ?)`;
+
+  conexao.query(sql, [nomeTarefa, prioridade], (error, results) => {
+    if (error) {
+      return res.status(500).send({
+        message: "Erro ao inserir tarefa: " + error,
+      });
+    }
+    return res.status(201).send({
+      message: "Tarefa inserida com sucesso!",
+      data: results,
+    });
+  });
 };
 
 const getAll = (req, res) => {
-    const sql = 'SELECT * FROM Tarefas';
-    conexao.query(sql, (error, results) => {
-        if (error) {
-            res.status(500).send("Erro ao buscar tarefas: " + error);
-        } else {
-            res.render('index', { tarefas: results });
-        }
-    });
+  const sql = "SELECT * FROM Tarefas";
+  conexao.query(sql, (error, results) => {
+    if (error) {
+      res.status(500).send("Erro ao buscar tarefas: " + error);
+    } else {
+      res.render("index", { tarefas: results });
+    }
+  });
 };
-
 
 module.exports = {
-
-  inserirTarefa, getAll
+  inserirTarefa,
+  getAll,
 };
-
